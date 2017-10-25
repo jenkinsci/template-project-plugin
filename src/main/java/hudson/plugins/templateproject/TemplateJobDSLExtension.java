beeee4a9d5e5c2e8982066ba5bf9fc9ae8d621d3
Mads Nielsen <inkspot84@gmail.com>
2017-10-25 20:41:44 +0200

    Don't know the next version number

--- src/main/java/hudson/plugins/templateproject/TemplateJobDSLExtension.java
+++ src/main/java/hudson/plugins/templateproject/TemplateJobDSLExtension.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hudson.plugins.templateproject;
 
 import hudson.Extension;
@@ -15,31 +10,28 @@ import javaposse.jobdsl.dsl.helpers.wrapper.WrapperContext;
 
 import javaposse.jobdsl.plugin.ContextExtensionPoint;
 
-/*
- * @author Mads
- */
 @Extension(optional = true)
 public class TemplateJobDSLExtension extends ContextExtensionPoint  {
     
-    @RequiresPlugin(id = "template-project", minimumVersion = "1.5.2")
+    @RequiresPlugin(id = "template-project")
     @DslExtensionMethod(context = StepContext.class)
     public Object templateBuilder(String jobName) {
         return new ProxyBuilder(jobName);
     }
     
-    @RequiresPlugin(id = "template-project", minimumVersion = "1.5.2")
+    @RequiresPlugin(id = "template-project")
     @DslExtensionMethod(context = PublisherContext.class)    
     public Object templatePublisher(String jobName){
         return new ProxyPublisher(jobName);
     }
     
-    @RequiresPlugin(id = "template-project", minimumVersion = "1.5.2")
+    @RequiresPlugin(id = "template-project")
     @DslExtensionMethod(context = WrapperContext.class)    
     public Object templateBuildEnv(String jobName){
         return new ProxyBuildEnvironment(jobName);
     }    
     
-    @RequiresPlugin(id = "template-project", minimumVersion = "1.5.2")
+    @RequiresPlugin(id = "template-project")
     @DslExtensionMethod(context = ScmContext.class)    
     public Object templateScm(String jobName){
         return new ProxySCM(jobName);
